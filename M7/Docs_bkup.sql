89d20c81e587a685a9b794160357448aab83260b
Vitaly Morozov <morozv.vitaly@gmail.com>
2022-05-19 11:00:56 +0200

    new file:   M7/Images/7.18.png

---
+++ M7/Docs_bkup.sql
@@ -0,0 +1,128 @@
+-- MySQL dump 10.13  Distrib 5.7.38, for Linux (x86_64)
+--
+-- Host: localhost    Database: Docs
+-- ------------------------------------------------------
+-- Server version	5.7.38-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Ambulatories`
+--
+
+DROP TABLE IF EXISTS `Ambulatories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Ambulatories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `specialization` varchar(40) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ambulatories`
+--
+
+LOCK TABLES `Ambulatories` WRITE;
+/*!40000 ALTER TABLE `Ambulatories` DISABLE KEYS */;
+INSERT INTO `Ambulatories` VALUES (1,'Central # 1','Surgery',2000),(2,'Central # 2','Pediatrics',1500),(3,'Region #1 ','General',500);
+/*!40000 ALTER TABLE `Ambulatories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Cities`
+--
+
+DROP TABLE IF EXISTS `Cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `postindex` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cities`
+--
+
+LOCK TABLES `Cities` WRITE;
+/*!40000 ALTER TABLE `Cities` DISABLE KEYS */;
+INSERT INTO `Cities` VALUES (1,'Kramatorsk',84300),(2,'Slavyansk',84400),(3,'Konstantinovka',84200),(4,'Druzhkovka',84500);
+/*!40000 ALTER TABLE `Cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Doctors`
+--
+
+DROP TABLE IF EXISTS `Doctors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Doctors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `profile` varchar(40) DEFAULT NULL,
+  `CID` int(11) DEFAULT NULL,
+  `AID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Doctors`
+--
+
+LOCK TABLES `Doctors` WRITE;
+/*!40000 ALTER TABLE `Doctors` DISABLE KEYS */;
+INSERT INTO `Doctors` VALUES (1,'Ivanov I','surgeon',NULL,2),(2,'Petrov V','surgeon',1,2),(3,'Sidorov P','pediatrician',3,3);
+/*!40000 ALTER TABLE `Doctors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Nurse`
+--
+
+DROP TABLE IF EXISTS `Nurse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Nurse` (
+  `id` int(11) DEFAULT NULL,
+  `Name` varchar(40) DEFAULT NULL,
+  `aid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Nurse`
+--
+
+LOCK TABLES `Nurse` WRITE;
+/*!40000 ALTER TABLE `Nurse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Nurse` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-18 14:48:56
